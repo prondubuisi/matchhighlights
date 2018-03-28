2fc55b711c2415f89b8f3bce8b96a0fc4cf7f438
NdubuisiO <onyemenamndu@gmail.com>
2018-03-28 22:09:25 +0100

    change page access token

--- src/Config.php
+++ src/Config.php
@@ -9,9 +9,7 @@ Class Config{
    static $facebookCredentials = [
 
        'verifyToken' => 'politricks',
-       'accessToken' => 'EAALGjd6t0LkBAFip2VoG1LJH2jNETHTHJZAP7TGsBqg7ioH9ECIqzZCEZCZBPkPBZCssvrUR1t57qRFcLth5Bjm16C61lOI2TNdyM407gtaB2EyvJJ2ykdQ4XpdU22A6KvUzKBkGz2NjqKLWdo45Mnp8u2HSOGKCwLKwYwthzdAZDZD'
-
-   ];
+       'accessToken' => 'EAALGjd6t0LkBAKLO8atCBbftZBLOHHgNOIM8Ehfyr1j5GRQZCQ42YeaJBMly6Gt8A1bePwZC2n5Bbr6xkvzceLGar4u5KBwHdTDKK8tZAxkmZBmN2WNevfCQxzKbFaAy9N2Flv5WZAd4uZCbVx3oVK3TrISfVk7XyrsEnBuKI8B9RV35NN7d8ED' ];
 
     
     static function getVerifyToken()
