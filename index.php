908a3d423b505902537cc3f3bb5db55a3363d019
NdubuisiO <onyemenamndu@gmail.com>
2018-03-29 05:36:05 +0100

    Changed response

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $sendMessage = new SendMessage($input);
 if(!empty($receivedMessage->textMessage))
 {
      
-      $sendMessage->text("David Cool down na");
+      $sendMessage->text("David Cool down na, error prone");
 
 }
  
