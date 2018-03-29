9660c957aa1d5923e9172d2468298a5a610ccd57
NdubuisiO <onyemenamndu@gmail.com>
2018-03-29 05:50:21 +0100

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -9,7 +9,7 @@ Class Config{
    static $facebookCredentials = [
 
        'verifyToken' => 'politricks',
-       'accessToken' => 'EAALGjd6t0LkBAGyvY7WaDX9EyB0QsG2DcNOwZCgiQ41lU432WoxqEm5Flzmhmbt8dmQiAjmtf0r80xucnU7KDPSZCEmSZCIV5vhO1vuLH5rr7JQ0ZAtEXTa1NCxrloC9cBDjZARlmQ4JNa4lnhegIaEt28oAeh5OlJViCs9ZAlVoYj8pcFkIYM';   
+       'accessToken' => 'EAALGjd6t0LkBAGyvY7WaDX9EyB0QsG2DcNOwZCgiQ41lU432WoxqEm5Flzmhmbt8dmQiAjmtf0r80xucnU7KDPSZCEmSZCIV5vhO1vuLH5rr7JQ0ZAtEXTa1NCxrloC9cBDjZARlmQ4JNa4lnhegIaEt28oAeh5OlJViCs9ZAlVoYj8pcFkIYM'   
     static function getVerifyToken()
     {
 
