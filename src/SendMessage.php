629a2549e675f28d780bc33eb2749248603933a7
NdubuisiO <onyemenamndu@gmail.com>
2018-04-03 22:29:38 +0100

    Update SendMessage.php

--- src/SendMessage.php
+++ src/SendMessage.php
@@ -39,6 +39,53 @@ class SendMessage{
 
    
     }
+	
+    public function carousel() {
+	    
+	      $userID = $this->userID;
+    
+    	$response = [
+  "recipient"=>[
+    "id"=> $userID
+  ],
+  "message"=>[
+    "attachment"=>[
+      "type"=>"template",
+      "payload"=>[
+        "template_type"=>"generic",
+        "elements"=>[
+           [
+            "title"=>"Welcome!",
+            "image_url"=>"https=>//petersfancybrownhats.com/company_image.png",
+            "subtitle"=>"We have the right hat for everyone.",
+            "default_action"=> [
+              "type"=> "web_url",
+              "url"=> "https=>//petersfancybrownhats.com/view?item=103",
+              "messenger_extensions"=> false,
+              "webview_height_ratio"=> "tall",
+              "fallback_url"=> "https=>//petersfancybrownhats.com/"
+            ],
+            "buttons"=>[
+              [
+                "type"=>"web_url",
+                "url"=>"https=>//petersfancybrownhats.com",
+                "title"=>"View Website"
+              ],[
+                "type"=>"postback",
+                "title"=>"Start Chatting",
+                "payload"=>"DEVELOPER_DEFINED_PAYLOAD"
+              ]              
+            ]      
+          ]
+        ]
+      ]
+    ]
+  ]
+];
+    
+	    $this->curlResponse($response);
+    
+    }
 
     public function curlResponse($response)
     {
