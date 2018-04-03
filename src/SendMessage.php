d15f2178528db098dccd6db2797017a1067bb3de
NdubuisiO <onyemenamndu@gmail.com>
2018-04-03 22:36:48 +0100

    Update SendMessage.php

--- src/SendMessage.php
+++ src/SendMessage.php
@@ -56,19 +56,19 @@ class SendMessage{
         "elements"=>[
            [
             "title"=>"Welcome!",
-            "image_url"=>"https=>//petersfancybrownhats.com/company_image.png",
+            "image_url"=>"https://petersfancybrownhats.com/company_image.png",
             "subtitle"=>"We have the right hat for everyone.",
             "default_action"=> [
               "type"=> "web_url",
-              "url"=> "https=>//petersfancybrownhats.com/view?item=103",
+              "url"=> "https://petersfancybrownhats.com/view?item=103",
               "messenger_extensions"=> false,
               "webview_height_ratio"=> "tall",
-              "fallback_url"=> "https=>//petersfancybrownhats.com/"
+              "fallback_url"=> "https://petersfancybrownhats.com/"
             ],
             "buttons"=>[
               [
                 "type"=>"web_url",
-                "url"=>"https=>//petersfancybrownhats.com",
+                "url"=>"https://petersfancybrownhats.com",
                 "title"=>"View Website"
               ],[
                 "type"=>"postback",
